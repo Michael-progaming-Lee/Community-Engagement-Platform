6508c2c701b56959fefca522cfd45cd99fb81e1e
Michael-progaming-Lee <lee.spike2001@gmail.com>
2024-11-09 21:55:42 -0500

    yes commit

--- register.php
+++ register.php
@@ -28,16 +28,14 @@
 
                 if (mysqli_num_rows($verify_query) != 0) {
                     echo "<div class='message'>
-                      <p>This email is used, Try another One Please!</p>
-                  </div> <br>";
+                    <p>This email is used, Try another One Please!</p> </div> <br>";
                     echo "<a href='javascript:self.history.back()'><button class='btn'>Go Back</button>";
                 } else {
 
                     mysqli_query($con, "INSERT INTO users(Username,Email,Age,Password) VALUES('$username','$email','$age','$password')") or die("Erroe Occured");
 
                     echo "<div class='message'>
-                      <p>Registration successfully!</p>
-                  </div> <br>";
+                    <p>Registration successfully!</p> </div> <br>";
                     echo "<a href='index.php'><button class='btn'>Login Now</button>";
                 }
             } else {
--- sql/tutorial.sql
+++ sql/tutorial.sql
@@ -1,7 +1,27 @@
+CREATE DATABASE IF NOT EXISTS community_engagement_db;
+
+-- Create user table
 CREATE TABLE users(
     Id int PRIMARY KEY AUTO_INCREMENT,
     Username varchar(200),
     Email varchar(200),
     Age int,
     Password varchar(200)
+);
+
+-- Create discussion table with foreign key linking to user
+CREATE TABLE discussion (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    author VARCHAR(200) NOT NULL,
+    content TEXT NOT NULL,
+);
+
+-- Create comments table with foreign key linking to discussion
+CREATE TABLE comments (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    discussion_id INT,
+    author VARCHAR(255),
+    comment TEXT,
+    FOREIGN KEY (discussion_id) REFERENCES discussion(id)
 );
